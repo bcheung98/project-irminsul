57bc908399fd8e85fb9bea36fe699c443a0048c6
Brian <briantcheung@gmail.com>
2023-01-11 17:23:51 -0800

    First commit

---
+++ src/components/Nav.js
@@ -0,0 +1,44 @@
+import * as React from "react";
+import { AppBar, Box, Toolbar, Typography, Container, Button, Tooltip } from "@mui/material";
+
+const pages = ["characters", "weapons"];
+
+const Nav = () => {
+
+    return (
+        <AppBar position="static" sx={{borderBottom: "2px solid rgb(18, 45, 73)"}}>
+            <Container maxWidth="xl">
+                <Toolbar disableGutters>
+                    <Typography
+                        variant="h6"
+                        noWrap
+                        component="a"
+                        href="/"
+                        sx={{
+                            mr: 2,
+                            display: { xs: "none", md: "flex" },
+                            fontFamily: "Genshin",
+                            letterSpacing: ".3rem",
+                            color: "inherit",
+                            textDecoration: "none",
+                        }}
+                    >
+                        PROJECT IRMINSUL
+                    </Typography>
+                    <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+                        {pages.map((page) => (
+                            <Button
+                                href={`/${page}`}
+                                key={page}
+                                sx={{ mx: 3, my: 2, color: "white", display: "block", fontFamily: "Genshin" }}
+                            >
+                                {page}
+                            </Button>
+                        ))}
+                    </Box>
+                </Toolbar>
+            </Container>
+        </AppBar>
+    );
+}
+export default Nav;
