cc61d1f37be202e89dafb8d7fe09a6b5b42f9cd7
Brian <briantcheung@gmail.com>
2023-01-13 17:08:14 -0800

    Added misc character info: VAs, constellation name, birthday, and release date

--- src/components/characters/page/_CharacterPage.js
+++ src/components/characters/page/_CharacterPage.js
@@ -1,6 +1,6 @@
 import * as React from "react";
 import { connect } from "react-redux";
-import { Typography, Avatar } from "@mui/material";
+import { Typography, Avatar, Paper } from "@mui/material";
 import Grid from "@mui/material/Unstable_Grid2";
 import { useParams } from "react-router-dom";
 import { MaterialTooltip } from "../../../helpers/MaterialTooltip";
@@ -13,19 +13,50 @@ const CharacterPage = (props) => {
     let character = characters.characters.find(char => char.name.split(" ").join("_").toLowerCase() === char_name);
 
     if (character !== undefined) {
-        let { name, title, rarity, element, weapon, constellation, description, birthday, nation, voiceActors } = character;
+        let { name, title, rarity, element, weapon, constellation, description, birthday, nation, voiceActors, release } = character;
         return (
             <React.Fragment>
                 <Grid container>
                     <Grid xs="auto">
-                        <img src={require(`../../../assets/characters/wish/Character_${name.split(" ").join("_")}_Wish.png`)} alt={name} style={{
-                            width: "40vw",
-                            height: "600px",
-                            objectFit: "cover",
-                            borderRight: "1px solid rgb(30, 73, 118)",
-                            borderBottom: "1px solid rgb(30, 73, 118)",
-                            borderBottomRightRadius: "5px",
-                        }} />
+                        <img src={require(`../../../assets/characters/wish/Character_${name.split(" ").join("_")}_Wish.png`)} alt={name}
+                            style={{
+                                width: "35vw",
+                                height: "600px",
+                                objectFit: "cover",
+                                marginLeft: "15px",
+                                borderLeft: "1px solid rgb(30, 73, 118)",
+                                borderRight: "1px solid rgb(30, 73, 118)",
+                                borderBottom: "1px solid rgb(30, 73, 118)",
+                                borderRadius: "0px 0px 5px 5px"
+                            }} />
+                        <Paper
+                            sx={{
+                                backgroundColor: "rgb(7, 27, 47)",
+                                border: "1px solid rgb(30, 73, 118)",
+                                borderRadius: "5px", color: "white",
+                                display: "flex",
+                                justifyContent: "space-between",
+                                ml: "15px",
+                                mt: "10px",
+                                px: "20px",
+                                py: "10px",
+                            }}>
+                            <div>
+                                <Typography variant="body2" sx={{ fontFamily: "Genshin" }}>Voice Actors:</Typography>
+                                <Typography variant="body2"><b>EN:</b> {voiceActors["en"]}</Typography>
+                                <Typography variant="body2"><b>JP:</b> {voiceActors["jp"]}</Typography>
+                            </div>
+                            <div style={{ marginLeft: "100px" }}>
+                                <Typography variant="body2" sx={{ fontFamily: "Genshin" }}>Constellation:</Typography>
+                                <Typography variant="body2" sx={{ fontFamily: "Genshin" }}>Birthday:</Typography>
+                                <Typography variant="body2" sx={{ fontFamily: "Genshin" }}>Release Date:</Typography>
+                            </div>
+                            <div>
+                                <Typography variant="body2" sx={{ fontFamily: "Genshin" }}>{constellation.name}</Typography>
+                                <Typography variant="body2" sx={{ fontFamily: "Genshin" }}>{birthday}</Typography>
+                                <Typography variant="body2" sx={{ fontFamily: "Genshin" }}>{`${release.date} (${release.version})`}</Typography>
+                            </div>
+                        </Paper>
                     </Grid>
                     <Grid xs>
                         <div style={{ display: "flex" }}>
@@ -66,7 +97,7 @@ const CharacterPage = (props) => {
                         <Typography
                             variant="body1"
                             noWrap
-                            component="a"
+                            component="p"
                             sx={{
                                 mx: "25px",
                                 mt: "5px",
@@ -79,14 +110,15 @@ const CharacterPage = (props) => {
                         >
                             {title}
                         </Typography>
-                        <img style={{
-                            marginLeft: "20px",
-                            marginTop: "2px",
-                        }} src={require(`../../../assets/stars/Icon_${rarity}_Stars.png`)} alt={rarity} />
+                        <img
+                            style={{
+                                marginLeft: "20px",
+                                marginTop: "2px",
+                            }} src={require(`../../../assets/stars/Icon_${rarity}_Stars.png`)} alt={rarity} />
                         <br /><br />
                         <Typography
                             variant="body2"
-                            component="a"
+                            component="p"
                             sx={{
                                 mx: "25px",
                                 mt: "5px",
