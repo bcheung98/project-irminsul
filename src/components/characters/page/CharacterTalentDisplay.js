4fb49b9015e498278e2e525d879bd1a987262b4a
Brian <briantcheung@gmail.com>
2023-01-13 23:11:18 -0800

    Added talent scaling table

--- src/components/characters/page/CharacterTalentDisplay.js
+++ src/components/characters/page/CharacterTalentDisplay.js
@@ -1,8 +1,46 @@
 import * as React from "react";
+import { styled } from '@mui/material/styles';
 import parse from "html-react-parser";
-import { Typography, Box, Avatar, CardHeader } from "@mui/material";
+import { Typography, Box, Avatar, CardHeader, Paper } from "@mui/material";
+import MuiAccordion from '@mui/material/Accordion';
+import MuiAccordionSummary from '@mui/material/AccordionSummary';
+import MuiAccordionDetails from '@mui/material/AccordionDetails';
+import ArrowForwardIosSharpIcon from '@mui/icons-material/ArrowForwardIosSharp';
 import { FormatTalentKey } from "../../../helpers/FormatTalentKey";
 import { ElementalBorderColor } from "../../../helpers/ElementalColors";
+import CharacterTalentScalingTable from "./CharacterTalentScalingTable";
+
+const Accordion = styled((props) => (
+    <MuiAccordion disableGutters elevation={0} {...props} />
+))(() => ({
+    '&:not(:last-child)': {
+        borderBottom: 0,
+    },
+    '&:before': {
+        display: 'none',
+    },
+}));
+
+const AccordionSummary = styled((props) => (
+    <MuiAccordionSummary
+        expandIcon={<ArrowForwardIosSharpIcon sx={{ fontSize: '0.9rem', color: "dodgerblue" }} />}
+        {...props}
+    />
+))(() => ({
+    backgroundColor: "rgb(9, 24, 39)",
+    flexDirection: 'row-reverse',
+    '& .MuiAccordionSummary-expandIconWrapper.Mui-expanded': {
+        transform: 'rotate(90deg)',
+    },
+    '& .MuiAccordionSummary-content': {
+        marginLeft: "10px",
+    },
+}));
+
+const AccordionDetails = styled(MuiAccordionDetails)(() => ({
+    backgroundColor: "rgb(9, 24, 39)",
+    padding: "10px",
+}));
 
 const CharacterTalentDisplay = (props) => {
 
@@ -46,7 +84,6 @@ const CharacterTalentDisplay = (props) => {
                                         sx={{
                                             display: "flex",
                                             flexDirection: "column",
-                                            mr: "10px",
                                             width: "48px",
                                             height: "48px",
                                             border: "2px solid rgb(30, 73, 118)",
@@ -56,30 +93,46 @@ const CharacterTalentDisplay = (props) => {
                                         sx={{
                                             display: "flex",
                                             flexDirection: "column",
-                                            mr: "10px",
                                             width: "48px",
                                             height: "48px",
                                             border: "2px solid rgb(30, 73, 118)",
                                         }} />
                             }
                             title={
-                                <Typography variant="h6" sx={{ fontFamily: "Genshin, sans-serif", }}>
+                                <Typography variant="h6" sx={{ fontFamily: "Genshin, sans-serif" }}>
                                     {talents[key].name}
                                 </Typography>
                             }
                         />
-                        <Typography variant="body1" sx={{ fontSize: "16px" }}>
+                        <Typography variant="body1" sx={{ fontSize: "16px", ml: "20px" }}>
                             {parse(talents[key].description)}
                         </Typography>
                         {
                             talents[key].splash &&
                             <React.Fragment>
                                 <br />
-                                <i style={{ fontSize: "15px", color: "rgb(225, 225, 225)" }}>{parse(talents[key].splash)}</i>
+                                <i style={{ fontSize: "15px", color: "rgb(225, 225, 225)", marginLeft: "20px" }}>{parse(talents[key].splash)}</i>
                                 <br />
                             </React.Fragment>
                         }
-                        <br /><hr style={{ border: ".5px solid rgb(30, 73, 118)" }} /><br />
+                        <br />
+                        {
+                            ["attack", "skill", "burst", "altsprint"].includes(key) &&
+                            <Paper variant="outlined" sx={{
+                                color: "white",
+                                border: "none",
+                            }}>
+                                <Accordion>
+                                    <AccordionSummary>
+                                        <Typography variant="body1" sx={{ color: "white", fontWeight: "bold" }}>Talent Scaling</Typography>
+                                    </AccordionSummary>
+                                    <AccordionDetails>
+                                        <CharacterTalentScalingTable attackType={key} stats={talents[key].scaling} />
+                                    </AccordionDetails>
+                                </Accordion>
+                            </Paper>
+                        }
+                        <hr style={{ border: ".5px solid rgb(30, 73, 118)", marginTop: "15px", marginBottom: "15px" }} />
                     </Box>
                 )
             })}
