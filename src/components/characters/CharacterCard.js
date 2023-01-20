c800b2c61ea09994f97d3cbdf67308c676aa2dbc
Brian <briantcheung@gmail.com>
2023-01-20 01:36:30 -0800

    Optimized character card code

--- src/components/characters/CharacterCard.js
+++ src/components/characters/CharacterCard.js
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { Typography, Card, CardContent, Avatar, ButtonBase } from "@mui/material";
+import { Typography, Card, CardContent, Avatar, ButtonBase, Box } from "@mui/material";
 import Grid from "@mui/material/Unstable_Grid2";
 import CharacterMaterialGrid from "./CharacterMaterialGrid";
 import { MaterialTooltip } from "../../helpers/MaterialTooltip";
@@ -15,82 +15,74 @@ const CharacterCard = (props) => {
     return (
         <React.Fragment>
             <Card variant="outlined" sx={{
-                width: 325,
-                height: 200,
-                margin: "auto",
-                mb: "18px",
+                width: 320,
+                height: 185,
+                mx: "auto",
+                my: "10px",
                 backgroundColor: "rgb(0, 30, 60)",
                 border: "1px solid rgb(30, 73, 118)",
                 borderRadius: "5px",
                 fontFamily: "Genshin, sans-serif"
             }}>
-                <CardContent sx={{
-                    mt: "-8px",
-                }}>
-                    <div id="top-row">
-                        <Typography sx={{
-                            fontFamily: "Genshin, sans-serif",
-                            color: "white",
-                            ml: "-5px",
-                        }} variant="h5">
-                            {name}
-                        </Typography>
-                        <div id="circle-icons" style={{
+                <CardContent sx={{ py: "10px" }}>
+                    <Box
+                        sx={{
                             display: "flex",
-                            position: "relative",
-                            marginTop: "-35px",
-                            marginBottom: "5px",
-                            marginLeft: "225px",
+                            position: "relative"
                         }}>
+                        <ButtonBase disableRipple href={`/project-irminsul/character/${props.character.name.split(" ").join("_").toLowerCase()}`} target="_blank">
+                            <Typography sx={{
+                                fontFamily: "Genshin, sans-serif",
+                                color: "white",
+                            }} variant="h5">
+                                {name}
+                            </Typography>
+                        </ButtonBase>
+                        <Box
+                            sx={{
+                                display: "flex",
+                                position: "absolute",
+                                right: "-5px"
+                            }}
+                        >
                             <MaterialTooltip title={element} arrow placement="top">
                                 <Avatar sx={{
-                                    height: "35px",
-                                    width: "35px",
+                                    height: "30px",
+                                    width: "30px",
                                 }} src={(`${process.env.REACT_APP_URL}/elements/Element_${element}.png`)} alt={element} />
                             </MaterialTooltip>
                             <MaterialTooltip title={weapon} arrow placement="top">
                                 <Avatar sx={{
-                                    height: "35px",
-                                    width: "35px"
+                                    height: "30px",
+                                    width: "30px"
                                 }} src={(`${process.env.REACT_APP_URL}/weapons/icons/Weapon-class-${weapon.toLowerCase()}-icon.png`)} alt={weapon} />
                             </MaterialTooltip>
-                        </div>
-                    </div>
-                    <Grid container spacing={2}>
-                        <Grid item xs sx={{
-                            marginTop: "5px",
-                            marginLeft: "-40px",
-                            width: "100px",
-                        }}>
-                            <Avatar variant="square" sx={{
-                                margin: "auto",
-                                border: "1px solid rgb(30, 73, 118)",
-                                borderRadius: "5px",
-                                width: '90px',
-                                height: '90px',
-                                backgroundColor: "rgb(32, 32, 32)",
-                            }} src={(`${process.env.REACT_APP_URL}/characters/thumbs/Character_${name.split(" ").join("_")}_Thumb.png`)} alt={name} style={characterIconBackground} />
+                        </Box>
+                    </Box>
+                    <Grid container sx={{ mt: "10px" }}>
+                        <Grid xs>
+                            <ButtonBase disableRipple href={`/project-irminsul/character/${props.character.name.split(" ").join("_").toLowerCase()}`} target="_blank">
+                                <Avatar variant="square" sx={{
+                                    margin: "auto",
+                                    ml: "2px",
+                                    border: "1px solid rgb(30, 73, 118)",
+                                    borderRadius: "5px",
+                                    width: "90px",
+                                    height: "90px",
+                                    backgroundColor: "rgb(32, 32, 32)",
+                                }} src={(`${process.env.REACT_APP_URL}/characters/thumbs/Character_${name.split(" ").join("_")}_Thumb.png`)} alt={name} style={characterIconBackground} />
+                            </ButtonBase>
                             <img style={{
                                 display: "block",
+                                margin: "auto",
+                                marginTop: "5px",
                                 height: "25px",
-                                marginLeft: "auto",
-                                marginRight: "auto",
-                                marginTop: "2px",
                             }} src={(`${process.env.REACT_APP_URL}/stars/Icon_${rarity}_Stars.png`)} alt={rarity} />
                         </Grid>
-                        <CharacterMaterialGrid character={props.character} imageSize="48px" margin="-30px" />
+                        <Grid xs={8}>
+                            <CharacterMaterialGrid character={props.character} />
+                        </Grid>
                     </Grid>
-                    <ButtonBase disableRipple href={`/project-irminsul/character/${props.character.name.split(" ").join("_").toLowerCase()}`} target="_blank">
-                        <Typography sx={{
-                            fontFamily: "Genshin, sans-serif",
-                            fontSize: "9pt",
-                            color: "rgb(10, 155, 201)",
-                            ml: "1px",
-                            mt: "5px",
-                        }}>
-                            MORE INFO
-                        </Typography>
-                    </ButtonBase>
                 </CardContent>
             </Card >
         </React.Fragment >
--- src/components/characters/CharacterMaterialGrid.js
+++ src/components/characters/CharacterMaterialGrid.js
@@ -2,49 +2,54 @@ import React from "react";
 import { formatTalents, formatCommonMats, formatBossMats, formatWeeklyBossMats, formatGemstone } from "../../helpers/TooltipText";
 import Grid from "@mui/material/Unstable_Grid2";
 import { MaterialTooltip } from "../../helpers/MaterialTooltip";
+import { Box } from "@mui/material";
 
 const CharacterMaterialGrid = (props) => {
     let { element } = props.character;
     let { talentBook, bossMat, localMat, commonMat, weeklyBossMat } = props.character.materials;
 
-    const materialRow = {
-        marginLeft: props.margin
-    }
-
     const materialImage = {
-        height: props.imageSize,
+        height: "48px",
         border: "1px solid rgb(30, 73, 118)",
         borderRadius: "5px",
-        marginBottom: "-14px",
-        marginRight: "7px",
         backgroundColor: "rgb(9, 24, 39)",
     }
 
     return (
-        <Grid item sm sx={{mt: "-3px"}}>
-            <Grid style={materialRow}>
-                <MaterialTooltip title={formatTalents(talentBook)} arrow placement="top">
-                    <img style={materialImage} src={(`${process.env.REACT_APP_URL}/materials/talent_mats/${talentBook}3.png`)} alt={talentBook} />
-                </MaterialTooltip>
-                <MaterialTooltip title={formatWeeklyBossMats(weeklyBossMat)} arrow placement="top">
-                    <img style={materialImage} src={(`${process.env.REACT_APP_URL}/materials/weekly_boss_mats/${weeklyBossMat.split(" ").join("_")}.png`)} alt={weeklyBossMat} />
-                </MaterialTooltip>
-                <MaterialTooltip title={formatCommonMats(commonMat)} arrow placement="top">
-                    <img style={materialImage} src={(`${process.env.REACT_APP_URL}/materials/common_mats/${commonMat.split(" ").join("_")}3.png`)} alt={commonMat} />
-                </MaterialTooltip>
+        <Box sx={{ ml: "10px" }}>
+            <Grid container spacing={1}>
+                <Grid xs="auto">
+                    <MaterialTooltip title={formatTalents(talentBook)} arrow placement="top">
+                        <img style={materialImage} src={(`${process.env.REACT_APP_URL}/materials/talent_mats/${talentBook}3.png`)} alt={talentBook} />
+                    </MaterialTooltip>
+                </Grid>
+                <Grid >
+                    <MaterialTooltip title={formatWeeklyBossMats(weeklyBossMat)} arrow placement="top">
+                        <img style={materialImage} src={(`${process.env.REACT_APP_URL}/materials/weekly_boss_mats/${weeklyBossMat.split(" ").join("_")}.png`)} alt={weeklyBossMat} />
+                    </MaterialTooltip>
+                </Grid>
+                <Grid>
+                    <MaterialTooltip title={formatCommonMats(commonMat)} arrow placement="top">
+                        <img style={materialImage} src={(`${process.env.REACT_APP_URL}/materials/common_mats/${commonMat.split(" ").join("_")}3.png`)} alt={commonMat} />
+                    </MaterialTooltip>
+                </Grid>
+                <Grid >
+                    <MaterialTooltip title={formatBossMats(bossMat)} arrow placement="top">
+                        <img style={materialImage} src={(`${process.env.REACT_APP_URL}/materials/boss_mats/${bossMat.split(" ").join("_")}.png`)} alt={bossMat} />
+                    </MaterialTooltip>
+                </Grid>
+                <Grid >
+                    <MaterialTooltip title={localMat} arrow placement="top">
+                        <img style={materialImage} src={(`${process.env.REACT_APP_URL}/materials/local_specialties/${localMat.split(" ").join("_")}.png`)} alt={localMat} />
+                    </MaterialTooltip>
+                </Grid>
+                <Grid>
+                    <MaterialTooltip title={formatGemstone(element)} arrow placement="top">
+                        <img style={materialImage} src={(`${process.env.REACT_APP_URL}/materials/ascension_gems/${element}_Gemstone.png`)} alt={element} />
+                    </MaterialTooltip>
+                </Grid>
             </Grid>
-            <Grid style={materialRow}>
-                <MaterialTooltip title={formatBossMats(bossMat)} arrow placement="top">
-                    <img style={materialImage} src={(`${process.env.REACT_APP_URL}/materials/boss_mats/${bossMat.split(" ").join("_")}.png`)} alt={bossMat} />
-                </MaterialTooltip>
-                <MaterialTooltip title={localMat} arrow placement="top">
-                    <img style={materialImage} src={(`${process.env.REACT_APP_URL}/materials/local_specialties/${localMat.split(" ").join("_")}.png`)} alt={localMat} />
-                </MaterialTooltip> 
-                <MaterialTooltip title={formatGemstone(element)} arrow placement="top">
-                    <img style={materialImage} src={(`${process.env.REACT_APP_URL}/materials/ascension_gems/${element}_Gemstone.png`)} alt={element} />
-                </MaterialTooltip>
-            </Grid>
-        </Grid>
+        </Box>
     )
 }
 
