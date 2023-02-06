fd3335724da199e9c261657315c4d10fa1ccbbf7
Brian <briantcheung@gmail.com>
2023-02-06 00:42:24 -0800

    Slightly decreased font size

--- src/components/characters/page/CharacterTalentDisplay.js
+++ src/components/characters/page/CharacterTalentDisplay.js
@@ -57,7 +57,7 @@ const CharacterTalentDisplay = (props) => {
                 padding: "15px",
             }}>
             <Typography
-                variant="h4"
+                variant="h5"
                 noWrap
                 component="p"
                 sx={{
@@ -70,9 +70,6 @@ const CharacterTalentDisplay = (props) => {
             {Object.keys(talents).map((key, index) => {
                 return (
                     <Box key={key}>
-                        <Typography variant="body1" sx={{ fontFamily: "Genshin, sans-serif", }}>
-                            <i>{FormatTalentKey(key).toUpperCase()}</i>
-                        </Typography>
                         <CardHeader
                             sx={{
                                 display: "flex",
@@ -99,19 +96,24 @@ const CharacterTalentDisplay = (props) => {
                                         }} />
                             }
                             title={
-                                <Typography variant="h6" sx={{ fontFamily: "Genshin, sans-serif" }}>
-                                    {talents[key].name}
-                                </Typography>
+                                <React.Fragment>
+                                    <Typography variant="h6" sx={{ fontFamily: "Genshin, sans-serif" }}>
+                                        {talents[key].name}
+                                    </Typography>
+                                    <Typography variant="subtitle2" sx={{ fontFamily: "Genshin, sans-serif", }}>
+                                        <i>{FormatTalentKey(key).toUpperCase()}</i>
+                                    </Typography>
+                                </React.Fragment>
                             }
                         />
-                        <Typography variant="body1" sx={{ fontSize: "16px", ml: "20px" }}>
+                        <Typography variant="body1" sx={{ fontSize: "15px", ml: "20px" }}>
                             {parse(talents[key].description)}
                         </Typography>
                         {
                             talents[key].splash &&
                             <React.Fragment>
                                 <br />
-                                <Typography variant="body1" sx={{ fontSize: "15px", fontStyle: "italic", ml: "20px", color: "rgb(225, 225, 225)" }}>
+                                <Typography variant="body1" sx={{ fontSize: "14px", fontStyle: "italic", ml: "20px", color: "rgb(225, 225, 225)" }}>
                                     {parse(talents[key].splash)}
                                 </Typography>
                             </React.Fragment>
--- src/helpers/StyledTable.js
+++ src/helpers/StyledTable.js
@@ -11,7 +11,7 @@ export const StyledTableCell = styled((props) => (
         fontFamily: "Genshin, sans-serif",
     },
     color: "white",
-    fontSize: "14px",
+    fontSize: "13.5px",
     fontFamily: "Roboto, sans-serif",
     border: "1px solid rgb(30, 73, 118)",
 }))
