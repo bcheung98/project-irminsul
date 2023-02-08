8ffe37253c3bd7b55507f0a4fcb8ca8ec63cee3d
Brian <briantcheung@gmail.com>
2023-02-07 18:13:18 -0800

    Optimized code for getting the character's crit rate, crit dmg, and ascension stat

--- src/components/characters/page/CharacterStatsTable.js
+++ src/components/characters/page/CharacterStatsTable.js
@@ -1,24 +1,28 @@
 import React from "react";
 import { Table, TableBody, TableContainer, TableHead, Paper } from "@mui/material";
 import { StyledTableCell, StyledTableRows } from "../../../helpers/StyledTable";
+import { CharacterAscensionStatScalings } from "../../../helpers/CharacterAscensionStatScalings";
 
 const createCharacterStats = (level, hp, atk, def, critRate, critDMG, special) => {
     return { level, hp, atk, def, critRate, critDMG, special }
 }
 
 const CharacterStatsTable = (props) => {
-    let { stats } = props.character;
+    let { rarity, stats } = props.character;
 
-    const levels = ["1", "20", "20+", "40", "40+", "50", "50+", "60", "60+", "70", "70+", "80", "80+", "90"]
-    const characterStatRows = levels.map((level, index) => stats.special ? createCharacterStats(level, stats.hp[index], stats.atk[index], stats.def[index], stats.critRate[index], stats.critDMG[index], stats.special[index]) : createCharacterStats(level, stats.hp[index], stats.atk[index], stats.def[index], stats.critRate[index], stats.critDMG[index]))
+    const levels = ["1", "20", "20+", "40", "40+", "50", "50+", "60", "60+", "70", "70+", "80", "80+", "90"];
+    const specialStats = CharacterAscensionStatScalings(rarity, stats.ascensionStat);
+    const characterStatRows = levels.map((level, index) => stats.special ? createCharacterStats(level, stats.hp[index], stats.atk[index], stats.def[index], specialStats.critRate[index], specialStats.critDMG[index], specialStats[stats.ascensionStat][index]) : createCharacterStats(level, stats.hp[index], stats.atk[index], stats.def[index], specialStats.critRate[index], specialStats.critDMG[index]));
 
     return (
-        <TableContainer sx={{
-            border: "2px solid rgb(30, 73, 118)",
-            borderRadius: "5px",
-            margin: "auto",
-            width: "95%",
-        }} component={Paper} >
+        <TableContainer
+            component={Paper}
+            sx={{
+                border: "2px solid rgb(30, 73, 118)",
+                borderRadius: "5px",
+                margin: "auto",
+                width: "95%",
+            }}>
             <Table sx={{ backgroundColor: "rgb(0, 30, 60)" }}>
                 <TableHead>
                     <StyledTableRows>
@@ -40,9 +44,9 @@ const CharacterStatsTable = (props) => {
                             <StyledTableCell>{Number(row.hp).toLocaleString()}</StyledTableCell>
                             <StyledTableCell>{row.atk}</StyledTableCell>
                             <StyledTableCell>{row.def}</StyledTableCell>
-                            <StyledTableCell>{row.critRate}%</StyledTableCell>
-                            <StyledTableCell>{row.critDMG}%</StyledTableCell>
-                            {stats.special && <StyledTableCell>{stats.ascensionStat !== "Elemental Mastery" ? row.special + "%" : row.special}</StyledTableCell>}
+                            <StyledTableCell>{row.critRate}</StyledTableCell>
+                            <StyledTableCell>{row.critDMG}</StyledTableCell>
+                            {stats.special && <StyledTableCell>{row.special}</StyledTableCell>}
                         </StyledTableRows>
                     ))}
                 </TableBody>
---
+++ src/helpers/CharacterAscensionStatScalings.js
@@ -0,0 +1,60 @@
+export const CharacterAscensionStatScalings = (rarity, ascensionStat) => {
+
+    // Default CRIT Rate and CRIT DMG scalings
+    let stats = {
+        "critRate": ["5%", "5%", "5%", "5%", "5%", "5%", "5%", "5%", "5%", "5%", "5%", "5%", "5%", "5%"],
+        "critDMG": ["50%", "50%", "50%", "50%", "50%", "50%", "50%", "50%", "50%", "50%", "50%", "50%", "50%", "50%"]
+    }
+
+    // CRIT Rate ascension
+    if (ascensionStat === "CRIT Rate") {
+        stats["critRate"] = ["5%", "5%", "5%", "5%", "9.8%", "9.8%", "14.6%", "14.6%", "14.6%", "14.6%", "19.4%", "19.4%", "24.2%", "24.2%"];
+    }
+
+    // CRIT DMG ascension
+    if (ascensionStat === "CRIT DMG") {
+        stats["critDMG"] = ["50%", "50%", "50%", "50%", "59.6%", "59.6%", "69.2%", "69.2%", "69.2%", "69.2%", "78.8%", "78.8%", "88.4%", "88.4%"];
+    }
+
+    // Healing Bonus ascension
+    if (ascensionStat === "Healing Bonus") {
+        stats[ascensionStat] = ["0%", "0%", "0%", "0%", "5.5%", "5.5%", "11.1%", "11.1%", "11.1%", "11.1%", "16.6%", "16.6%", "22.2%", "22.2%"];
+    }
+
+    // DEF or Physical DMG Bonus ascension
+    if (["DEF", "Physical DMG Bonus"].includes(ascensionStat)) {
+        stats[ascensionStat] = ["0%", "0%", "0%", "0%", "7.5%", "7.5%", "15%", "15%", "15%", "15%", "22.5%", "22.5%", "30%", "30%"];
+    }
+
+    // ATK, HP, or Elemental DMG Bonus ascension
+    if (["ATK", "HP", "Pyro DMG Bonus", "Hydro DMG Bonus", "Electro DMG Bonus", "Cryo DMG Bonus", "Anemo DMG Bonus", "Geo DMG Bonus", "Dendro DMG Bonus"].includes(ascensionStat)) {
+        if (rarity === 4) {
+            stats[ascensionStat] = ["0%", "0%", "0%", "0%", "6%", "6%", "12%", "12%", "12%", "12%", "18%", "18%", "24%", "24%"];
+        }
+        if (rarity === 5) {
+            stats[ascensionStat] = ["0%", "0%", "0%", "0%", "7.2%", "7.2%", "14.4%", "14.4%", "14.4%", "14.4%", "21.6%", "21.6%", "28.8%", "28.8%"];
+        }
+    }
+
+    // Energy Recharge ascension
+    if (ascensionStat === "Energy Recharge") {
+        if (rarity === 4) {
+            stats[ascensionStat] = ["0%", "0%", "0%", "0%", "6.7%", "6.7%", "13.3%", "13.3%", "13.3%", "13.3%", "20%", "20%", "26.7%", "26.7%"];
+        }
+        if (rarity === 5) {
+            stats[ascensionStat] = ["0%", "0%", "0%", "0%", "8%", "8%", "16%", "16%", "16%", "16%", "24%", "24%", "32%", "32%"];
+        }
+    }
+
+    // Elemental Mastery ascension
+    if (ascensionStat === "Elemental Mastery") {
+        if (rarity === 4) {
+            stats[ascensionStat] = [0, 0, 0, 0, 24, 24, 48, 48, 48, 48, 72, 72, 96, 96];
+        }
+        if (rarity === 5) {
+            stats[ascensionStat] = [0, 0, 0, 0, 28.8, 28.8, 57.6, 57.6, 57.6, 57.6, 86.4, 86.4, 115.2, 115.2];
+        }
+    }
+
+    return stats;
+}
