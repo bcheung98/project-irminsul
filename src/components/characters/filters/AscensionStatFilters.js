5f7749daf5c36b5c748070a2db3843d99a0cf4eb
Brian <briantcheung@gmail.com>
2023-02-28 12:54:04 -0800

    Added missing Healing Bonus filter

--- src/components/characters/filters/AscensionStatFilters.js
+++ src/components/characters/filters/AscensionStatFilters.js
@@ -2,14 +2,15 @@ import * as React from "react";
 import { connect } from "react-redux";
 import { FilterTooltip } from "../../../helpers/FilterTooltip";
 
-let hpIcon = (`${process.env.REACT_APP_URL}/icons/HP.png`);
 let atkIcon = (`${process.env.REACT_APP_URL}/icons/ATK.png`);
 let defIcon = (`${process.env.REACT_APP_URL}/icons/DEF.png`);
-let physicalIcon = (`${process.env.REACT_APP_URL}/icons/Physical DMG Bonus.png`);
-let erIcon = (`${process.env.REACT_APP_URL}/icons/Energy Recharge.png`);
+let hpIcon = (`${process.env.REACT_APP_URL}/icons/HP.png`);
 let crIcon = (`${process.env.REACT_APP_URL}/icons/CRIT_Rate.png`);
 let cdIcon = (`${process.env.REACT_APP_URL}/icons/CRIT_DMG.png`);
 let emIcon = (`${process.env.REACT_APP_URL}/icons/Elemental Mastery.png`);
+let erIcon = (`${process.env.REACT_APP_URL}/icons/Energy Recharge.png`);
+let hbIcon = (`${process.env.REACT_APP_URL}/icons/Healing Bonus.png`);
+let physicalIcon = (`${process.env.REACT_APP_URL}/icons/Physical DMG Bonus.png`);
 let pyroIcon = (`${process.env.REACT_APP_URL}/elements/Element_Pyro.png`);
 let hydroIcon = (`${process.env.REACT_APP_URL}/elements/Element_Hydro.png`);
 let electroIcon = (`${process.env.REACT_APP_URL}/elements/Element_Electro.png`);
@@ -21,14 +22,14 @@ let dendroIcon = (`${process.env.REACT_APP_URL}/elements/Element_Dendro.png`);
 const AscensionStatFilters = (props) => {
     return (
         <React.Fragment>
-            <FilterTooltip title="HP%" arrow placement="top">
-                <img className="filter-off" id="hp-button" src={hpIcon} alt="HP" onClick={(e) => props.setFilter(e.target.alt)} />
-            </FilterTooltip>
             <FilterTooltip title="ATK%" arrow placement="top">
                 <img className="filter-off" id="atk-button" src={atkIcon} alt="ATK" onClick={(e) => props.setFilter(e.target.alt)} />
             </FilterTooltip>
             <FilterTooltip title="DEF%" arrow placement="top">
                 <img className="filter-off" id="def-button" src={defIcon} alt="DEF" onClick={(e) => props.setFilter(e.target.alt)} />
+            </FilterTooltip>
+            <FilterTooltip title="HP%" arrow placement="top">
+                <img className="filter-off" id="hp-button" src={hpIcon} alt="HP" onClick={(e) => props.setFilter(e.target.alt)} />
             </FilterTooltip>
             <FilterTooltip title="CRIT Rate" arrow placement="top">
                 <img className="filter-off" id="crit rate-button" src={crIcon} alt="CRIT Rate" onClick={(e) => props.setFilter(e.target.alt)} />
@@ -36,11 +37,14 @@ const AscensionStatFilters = (props) => {
             <FilterTooltip title="CRIT DMG" arrow placement="top">
                 <img className="filter-off" id="crit dmg-button" src={cdIcon} alt="CRIT DMG" onClick={(e) => props.setFilter(e.target.alt)} />
             </FilterTooltip>
+            <FilterTooltip title="Elemental Mastery" arrow placement="top">
+                <img className="filter-off" id="elemental mastery-button" src={emIcon} alt="Elemental Mastery" onClick={(e) => props.setFilter(e.target.alt)} />
+            </FilterTooltip>
             <FilterTooltip title="Energy Recharge" arrow placement="top">
                 <img className="filter-off" id="energy recharge-button" src={erIcon} alt="Energy Recharge" onClick={(e) => props.setFilter(e.target.alt)} />
             </FilterTooltip>
-            <FilterTooltip title="Elemental Mastery" arrow placement="top">
-                <img className="filter-off" id="elemental mastery-button" src={emIcon} alt="Elemental Mastery" onClick={(e) => props.setFilter(e.target.alt)} />
+            <FilterTooltip title="Healing Bonus" arrow placement="top">
+                <img className="filter-off" id="healing bonus-button" src={hbIcon} alt="Healing Bonus" onClick={(e) => props.setFilter(e.target.alt)} />
             </FilterTooltip>
             <FilterTooltip title="Physical DMG Bonus" arrow placement="top">
                 <img className="filter-off" id="physical dmg bonus-button" src={physicalIcon} alt="Physical DMG Bonus" onClick={(e) => props.setFilter(e.target.alt)} />
