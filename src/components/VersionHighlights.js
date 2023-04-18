f13394bf20c25702fb8af85ebaab5ff39451f850
Brian <briantcheung@gmail.com>
2023-04-17 18:16:03 -0700

    Removed extra version

--- src/components/VersionHighlights.js
+++ src/components/VersionHighlights.js
@@ -31,9 +31,8 @@ const BootstrapInput = styled(InputBase)(({ theme }) => ({
 const VersionHighlights = (props) => {
 
     // MAKE SURE TO CHANGE THIS EVERY UPDATE!
-    let versions = ["3.7", "3.6", "3.5", "3.4", "3.3", "3.2", "3.1", "3.0", "2.8", "2.7", "2.6", "2.5", "2.4", "2.3", "2.2", "2.1", "2.0", "1.6", "1.5", "1.4", "1.3", "1.2", "1.1", "1.0"]
+    let versions = ["3.6", "3.5", "3.4", "3.3", "3.2", "3.1", "3.0", "2.8", "2.7", "2.6", "2.5", "2.4", "2.3", "2.2", "2.1", "2.0", "1.6", "1.5", "1.4", "1.3", "1.2", "1.1", "1.0"]
     let versionNames = [
-        "Version 3.7",
         "A Parade of Providence",
         "Windblume's Breath",
         "The Exquisite Night Chimes",
