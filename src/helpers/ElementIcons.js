a94ccedf55908c8cafdd153babb4f61c38380b81
Brian <briantcheung@gmail.com>
2024-02-12 01:00:07 -0800

    Added helper function for Element Icons

---
+++ src/helpers/ElementIcons.js
@@ -0,0 +1,20 @@
+import { Avatar } from "@mui/material";
+
+const ElementalIcon = { width: "32px", height: "32px" }
+
+export const Pyro = () => <Avatar src={`${process.env.REACT_APP_URL}/elements/Element_Pyro.png`} alt="Pyro" sx={ElementalIcon} />
+export const Hydro = () => <Avatar src={`${process.env.REACT_APP_URL}/elements/Element_Hydro.png`} alt="Hydro" sx={ElementalIcon} />
+export const Electro = () => <Avatar src={`${process.env.REACT_APP_URL}/elements/Element_Electro.png`} alt="Electro" sx={ElementalIcon} />
+export const Cryo = () => <Avatar src={`${process.env.REACT_APP_URL}/elements/Element_Cryo.png`} alt="Cryo" sx={ElementalIcon} />
+export const Anemo = () => <Avatar src={`${process.env.REACT_APP_URL}/elements/Element_Anemo.png`} alt="Anemo" sx={ElementalIcon} />
+export const Geo = () => <Avatar src={`${process.env.REACT_APP_URL}/elements/Element_Geo.png`} alt="Geo" sx={ElementalIcon} />
+export const Dendro = () => <Avatar src={`${process.env.REACT_APP_URL}/elements/Element_Dendro.png`} alt="Dendro" sx={ElementalIcon} />
+
+export const TCGPhysical = () => <Avatar src={`${process.env.REACT_APP_URL}/tcg/icons/elements/Physical.png`} alt="Physical" sx={ElementalIcon} />
+export const TCGPyro = () => <Avatar src={`${process.env.REACT_APP_URL}/tcg/icons/elements/Pyro.png`} alt="Pyro" sx={ElementalIcon} />
+export const TCGHydro = () => <Avatar src={`${process.env.REACT_APP_URL}/tcg/icons/elements/Hydro.png`} alt="Hydro" sx={ElementalIcon} />
+export const TCGElectro = () => <Avatar src={`${process.env.REACT_APP_URL}/tcg/icons/elements/Electro.png`} alt="Electro" sx={ElementalIcon} />
+export const TCGCryo = () => <Avatar src={`${process.env.REACT_APP_URL}/tcg/icons/elements/Cryo.png`} alt="Cryo" sx={ElementalIcon} />
+export const TCGAnemo = () => <Avatar src={`${process.env.REACT_APP_URL}/tcg/icons/elements/Anemo.png`} alt="Anemo" sx={ElementalIcon} />
+export const TCGGeo = () => <Avatar src={`${process.env.REACT_APP_URL}/tcg/icons/elements/Geo.png`} alt="Geo" sx={ElementalIcon} />
+export const TCGDendro = () => <Avatar src={`${process.env.REACT_APP_URL}/tcg/icons/elements/Dendro.png`} alt="Dendro" sx={ElementalIcon} />
