0d9b5aa544a03eabbff2a47746393e5e52f8438b
Brian <briantcheung@gmail.com>
2024-02-28 02:19:16 -0800

    Completely revamed outfit popup

--- src/components/characters/page/CharacterOutfitDisplay.js
+++ src/components/characters/page/CharacterOutfitDisplay.js
@@ -2,6 +2,7 @@ import * as React from "react";
 import { useTheme } from "@mui/material/styles";
 import parse from "html-react-parser";
 import { Typography, Box, Tabs } from "@mui/material";
+import Grid from "@mui/material/Unstable_Grid2";
 import { StyledTab, TabPanel } from "../../../helpers/CustomTabs";
 import ErrorLoadingImage from "../../../helpers/ErrorLoadingImage";
 
@@ -16,6 +17,24 @@ const CharacterOutfitDisplay = (props) => {
         setTabValue(newValue);
     }
 
+    const OutfitIcon = (rarity) => {
+        return {
+            width: "96px",
+            border: `2px solid ${theme.border.color}`,
+            borderRadius: "64px",
+            backgroundColor: `${theme.materialImage.backgroundColor}`,
+            backgroundImage: `url(${process.env.REACT_APP_URL}/backgrounds/Background_${rarity}_Star.png)`,
+            backgroundSize: "100%"
+        }
+    }
+
+    const OutfitSplash = {
+        width: "80%",
+        border: `1px solid ${theme.border.color}`,
+        borderRadius: "5px",
+        backgroundColor: `${theme.materialImage.backgroundColor}`
+    }
+
     return (
         <Box
             sx={{
@@ -25,70 +44,71 @@ const CharacterOutfitDisplay = (props) => {
                 borderRadius: "5px",
             }}
         >
-            <Tabs
-                variant="scrollable"
-                value={tabValue}
-                onChange={handleChangeTab}
-                sx={{ backgroundColor: `${theme.toolbar.backgroundColor}` }}
-            >
-                {outfits.map((outfit, index) => (
-                    <StyledTab key={index} label={outfit.name}
-                        sx={{
-                            fontFamily: "Genshin, sans-serif",
-                            fontSize: "14pt",
-                            color: `${theme.text.color}`,
-                            textTransform: "none",
-                        }}
-                    />
-                ))}
-            </Tabs>
-            {
-                outfits.map((outfit, index) => {
-                    return (
-                        <TabPanel
-                            key={index}
-                            index={index}
-                            value={tabValue}
-                        >
-                            <Box sx={{ marginLeft: "-5px" }}>
-                                <img style={{ height: "30px" }} src={(`${process.env.REACT_APP_URL}/stars/Icon_${outfit.rarity}_Stars.png`)} alt={outfit.rarity} onError={ErrorLoadingImage} />
-                            </Box>
-                            <Typography
-                                variant="body2"
-                                sx={{
-                                    mb: "20px",
-                                    fontFamily: "Genshin, sans-serif",
-                                    color: `${theme.text.color}`,
-                                }}
-                            >
-                                {parse(outfit.description)}
-                            </Typography>
-                            {
-                                index === 0 ?
-                                    <img src={(`${process.env.REACT_APP_URL}/characters/wish/Character_${name.split(" ").join("_")}_Wish.png`)} alt={outfit.name}
-                                        style={{
-                                            width: "95%",
-                                            border: `1px solid ${theme.border.color}`,
-                                            borderRadius: "5px",
-                                            backgroundColor: `${theme.materialImage.backgroundColor}`
+            <Grid container>
+                <Grid sx={{ backgroundColor: `${theme.toolbar.backgroundColor}`, width: "128px" }}>
+                    <Tabs
+                        orientation="vertical"
+                        variant="scrollable"
+                        scrollButtons="auto"
+                        value={tabValue}
+                        onChange={handleChangeTab}
+                    >
+                        {
+                            outfits.map((outfit, index) => (
+                                <StyledTab
+                                    key={index}
+                                    label={
+                                        index === 0 ?
+                                            <img src={`${process.env.REACT_APP_URL}/characters/thumbs/Character_${name.split(" ").join("_")}_Thumb.png`} alt={outfit.name} style={OutfitIcon(outfit.rarity)} onError={ErrorLoadingImage} />
+                                            :
+                                            <img src={`${process.env.REACT_APP_URL}/characters/outfits/icon/${outfit.name.split(" ").join("_")}_Icon.png`} alt={outfit.name} style={OutfitIcon(outfit.rarity)} onError={ErrorLoadingImage} />
+                                    }
+                                />
+                            ))
+                        }
+                    </Tabs>
+                </Grid>
+                <Grid xs>
+                    {
+                        outfits.map((outfit, index) => {
+                            return (
+                                <TabPanel
+                                    key={index}
+                                    index={index}
+                                    value={tabValue}
+                                >
+                                    <Typography
+                                        variant="h6"
+                                        sx={{
+                                            mb: "10px",
+                                            fontFamily: "Genshin, sans-serif",
+                                            color: `${theme.text.color}`,
                                         }}
-                                        onError={ErrorLoadingImage}
-                                    />
-                                    :
-                                    <img src={(`${process.env.REACT_APP_URL}/characters/outfits/Outfit_${outfit.name.split(" ").join("_")}_Preview.png`)} alt={outfit.name}
-                                        style={{
-                                            width: "95%",
-                                            border: `1px solid ${theme.border.color}`,
-                                            borderRadius: "5px",
-                                            backgroundColor: `${theme.materialImage.backgroundColor}`
+                                    >
+                                        {outfit.displayName ? outfit.displayName : outfit.name}
+                                    </Typography>
+                                    <Typography
+                                        variant="body2"
+                                        sx={{
+                                            mb: "20px",
+                                            fontFamily: "Genshin, sans-serif",
+                                            color: `${theme.text.color}`,
                                         }}
-                                        onError={ErrorLoadingImage}
-                                    />
-                            }
-                        </TabPanel>
-                    )
-                })
-            }
+                                    >
+                                        <i>{parse(outfit.description)}</i>
+                                    </Typography>
+                                    {
+                                        index === 0 ?
+                                            <img src={`${process.env.REACT_APP_URL}/characters/wish/Character_${name.split(" ").join("_")}_Wish.png`} alt={outfit.name} style={OutfitSplash} onError={ErrorLoadingImage} />
+                                            :
+                                            <img src={`${process.env.REACT_APP_URL}/characters/outfits/splash/${outfit.name.split(" ").join("_")}_Splash.png`} alt={outfit.name} style={OutfitSplash} onError={ErrorLoadingImage} />
+                                    }
+                                </TabPanel>
+                            )
+                        })
+                    }
+                </Grid>
+            </Grid>
         </Box>
     )
 }
