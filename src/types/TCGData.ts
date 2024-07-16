ed4d1121189cbfb7eef70ccf8bde46d2155bb35f
Brian <briantcheung@gmail.com>
2024-07-15 21:21:33 -0700

    Merged TCG data types into one file

--- src/types/TCGData.ts
+++ src/types/TCGData.ts
@@ -42,3 +42,27 @@ export interface TCGCharacterCardData {
         version: string
     }
 }
+
+export interface TCGActionCardData {
+    name: string,
+    displayName?: string,
+    type: string,
+    subType: string,
+    combatAction?: boolean,
+    character?: string,
+    cost: string,
+    description: string,
+    keywords: {
+        tag: string,
+        type?: string,
+        name: string,
+        description: string
+    }[],
+    splash: {
+        title: string,
+        description: string
+    },
+    release: {
+        version: string
+    }
+}
