283ea85fb761e0c5986c80dce750d3306052c2d0
Brian <briantcheung@gmail.com>
2024-07-22 19:31:27 -0700

    Changed variable name

--- src/Theme.ts
+++ src/Theme.ts
@@ -49,8 +49,10 @@ const defaultTheme = {
     }
 }
 
+const theme = defaultTheme
+
 type CustomTheme = {
-    [Key in keyof typeof defaultTheme]: typeof defaultTheme[Key]
+    [Key in keyof typeof theme]: typeof theme[Key]
 }
 
 declare module "@mui/material/styles" {
@@ -58,4 +60,4 @@ declare module "@mui/material/styles" {
     interface ThemeOptions extends CustomTheme { }
 }
 
-export default createTheme(defaultTheme)
+export default createTheme(theme)
