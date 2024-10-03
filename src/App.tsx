bbbd3a9ade549f89f13959ddc35b33ce3cc0b939
Brian <briantcheung@gmail.com>
2024-10-02 23:09:14 -0700

    Adding styling to main page box

--- src/App.tsx
+++ src/App.tsx
@@ -53,7 +53,7 @@ function App(props: any) {
 				<Box id="back-to-top-anchor" />
 				<Box sx={{ display: "flex" }}>
 					<Nav />
-					<Box sx={{ pt: 10, minHeight: "90vh" }}>
+					<Box sx={{ mx: "20px", mt: "100px", minHeight: "90vh", minWidth: "30vw", width: "95vw" }}>
 						<Switch>
 							<Route exact path="/" component={Home} />
 							<Route exact path="/characters" component={CharacterBrowser} />
