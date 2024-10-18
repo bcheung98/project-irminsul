cc107c6264dfc32289469f68457f28a3f33ce835
Brian <briantcheung@gmail.com>
2024-10-18 15:09:00 -0700

    Add border

--- src/components/_custom/CustomToggleButton.tsx
+++ src/components/_custom/CustomToggleButton.tsx
@@ -2,6 +2,7 @@ import { styled } from "@mui/material/styles"
 import { ToggleButton } from "@mui/material"
 
 export const CustomToggleButton = styled(ToggleButton)(({ theme }) => ({
+    border: `1px solid ${theme.border.color}`,
     "&.MuiToggleButton-root": {
         "&.Mui-selected": {
             backgroundColor: `${theme.button.selected}`,
