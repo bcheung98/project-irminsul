d493c4a4cb67f37120a0bd9c633fd9c42a40c840
Brian <briantcheung@gmail.com>
2024-10-21 00:51:37 -0700

    Added parser to date string before creating date object

--- src/helpers/dates.ts
+++ src/helpers/dates.ts
@@ -1,28 +1,30 @@
 const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
 
-const offset = ["UTC-5", "UTC+1", "UTC+8"] // NA, EU, Asia
+const offset = ["-5", "+1", "+8"] // NA, EU, Asia
 
 export function createDateObject(date: string) {
 
+    const formatDate = (date: string, offset: string) => `${date.split(" ").slice(0, 2).join(" ").replace(/-/g, "/")}${offset}`
+
     let dateObj
-    if (!date.endsWith("UTC+8")) {
-        dateObj = new Date(`${date} ${offset[0]}`)
+    if (date.endsWith("UTC+8")) {
+        dateObj = new Date(formatDate(date, "+8"))
     }
     else {
-        dateObj = new Date(`${date}`)
+        dateObj = new Date(formatDate(date, offset[0]))
     }
 
     let arr = dateObj.toLocaleString().split(",")
-
+    
     let dateArr = arr[0].split("/")
     let month = months[Number(dateArr[0]) - 1]
     let day = parseInt(dateArr[1], 10).toString()
     let year = dateArr[2]
     let dateString = `${month} ${day}, ${year}`
 
-    let timeArr = arr[1].split(" ")
-    let timestamp = `${timeArr[1].split(":").splice(0, 2).join(":")} ${timeArr[2]}`
-    
+    let timeArr = arr[1].trim().split(" ")
+    let timestamp = `${timeArr[0].split(":").splice(0, 2).join(":")} ${timeArr[1]}`
+
     return { obj: dateObj, date: dateString, time: timestamp }
 
 }
