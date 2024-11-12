6fa2249b5fbff6d46118a1e29238f3d5184294c3
Brian <briantcheung@gmail.com>
2024-11-12 14:55:39 -0800

    Update fetch

--- src/redux/actions/fetch.ts
+++ src/redux/actions/fetch.ts
@@ -1,9 +1,9 @@
 import { createAsyncThunk } from "@reduxjs/toolkit"
-import { CharacterData } from "../../types/character/CharacterData"
-import { WeaponData } from "../../types/weapon/WeaponData"
+import { Character } from "types/character"
+import { Weapon } from "types/weapon"
 import { TCGCardData } from "../../types/tcg/TCGData"
-import { ArtifactData } from "../../types/artifact/ArtifactData"
-import { BannerData, ChronicledWishBannerData } from "../../types/banner/BannerData"
+import { Artifact } from "types/artifact"
+import { Banner, ChronicledWishBanner } from "types/banner"
 
 // https://api.irminsul.gg/genshin/characters.json
 const CharactersURL = "https://api.irminsul.gg/genshin/characters.json"
@@ -21,12 +21,12 @@ const CharacterBannerURL = "https://api.irminsul.gg/genshin/character-banners.js
 const WeaponBannerURL = "https://api.irminsul.gg/genshin/weapon-banners.json"
 const ChronicledWishURL = "https://api.irminsul.gg/genshin/chronicled-wish.json"
 
-export const fetchCharacters = createAsyncThunk("GET/characters", async (): Promise<[CharacterData]> => {
+export const fetchCharacters = createAsyncThunk("GET/characters", async (): Promise<Character[]> => {
     const response = await fetch(CharactersURL)
     return await response.json()
 })
 
-export const fetchWeapons = createAsyncThunk("GET/weapons", async (): Promise<[WeaponData]> => {
+export const fetchWeapons = createAsyncThunk("GET/weapons", async (): Promise<Weapon[]> => {
     const response = await fetch(WeaponsURL)
     return await response.json()
 })
@@ -36,22 +36,22 @@ export const fetchCards = createAsyncThunk("GET/cards", async (): Promise<[TCGCa
     return await response.json()
 })
 
-export const fetchArtifacts = createAsyncThunk("GET/artifacts", async (): Promise<[ArtifactData]> => {
+export const fetchArtifacts = createAsyncThunk("GET/artifacts", async (): Promise<[Artifact]> => {
     const response = await fetch(ArtifactsURL)
     return await response.json()
 })
 
-export const fetchCharacterBanners = createAsyncThunk("GET/characterBanners", async (): Promise<[BannerData]> => {
+export const fetchCharacterBanners = createAsyncThunk("GET/characterBanners", async (): Promise<Banner[]> => {
     const response = await fetch(CharacterBannerURL)
     return await response.json()
 })
 
-export const fetchWeaponBanners = createAsyncThunk("GET/weaponBanners", async (): Promise<[BannerData]> => {
+export const fetchWeaponBanners = createAsyncThunk("GET/weaponBanners", async (): Promise<Banner[]> => {
     const response = await fetch(WeaponBannerURL)
     return await response.json()
 })
 
-export const fetchChronicledWish = createAsyncThunk("GET/chronicledWish", async (): Promise<[ChronicledWishBannerData]> => {
+export const fetchChronicledWish = createAsyncThunk("GET/chronicledWish", async (): Promise<ChronicledWishBanner[]> => {
     const response = await fetch(ChronicledWishURL)
     return await response.json()
 })
