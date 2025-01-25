c8698fc981b49ac3682fac8fdb206bd0dd1c9a70
Brian <briantcheung@gmail.com>
2025-01-24 18:33:39 -0800

    Revert passive text change

--- src/components/characters/page/CharacterPassives.tsx
+++ src/components/characters/page/CharacterPassives.tsx
@@ -11,7 +11,7 @@ import Grid from "@mui/material/Grid2";
 import { parseSkillDescription } from "helpers/parseSkillDescription";
 
 // Type imports
-import { CharacterProps } from "types/character";
+import { CharacterPassiveType, CharacterProps } from "types/character";
 
 function CharacterPassives({ character }: CharacterProps) {
     const theme = useTheme();
@@ -48,11 +48,10 @@ function CharacterPassives({ character }: CharacterProps) {
                             />
                             <Box>
                                 <TextStyled variant="h6-styled">
-                                    {`${
-                                        passive.type === "nightsoul"
-                                            ? "Night Realm's Gift: "
-                                            : ""
-                                    }${passive.name}`}
+                                    {passive.name}
+                                </TextStyled>
+                                <TextStyled sx={{ fontStyle: "italic" }}>
+                                    {formatPassiveKey(passive.type)}
                                 </TextStyled>
                             </Box>
                         </Stack>
@@ -72,3 +71,19 @@ function CharacterPassives({ character }: CharacterProps) {
 }
 
 export default CharacterPassives;
+
+function formatPassiveKey(type: CharacterPassiveType) {
+    switch (type) {
+        case "a1":
+            return "1st Ascension Passive";
+        case "a4":
+            return "4th Ascension Passive";
+        case "nightsoul":
+            return "Night Realm's Gift Passive";
+        case "util":
+            return "Utility Passive";
+        case "":
+        default:
+            return "Passive Talent";
+    }
+}
